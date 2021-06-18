10d335cf81390cdc76ad2ab024eed381ee18f49f
Cerebreo <nellirism@outlook.com>
2021-06-18 15:33:56 -0700

    dashboard for logged in users

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,27 @@
+const { JSONB } = require("sequelize/types");
+
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').nodeValue.trim();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
