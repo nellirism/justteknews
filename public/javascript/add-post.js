d8322f002f55edef24f50d7257b1383074a35dfb
Cerebreo <nellirism@outlook.com>
2021-06-19 15:13:02 -0700

    fixed content-type for application/json on add-post newformhandler

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,24 +2,27 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_url = document.querySelector('input[name="post_url"]').value;
+    const post_url = document.querySelector('input[name="post-url"]').value;
+    console.log(title, post_url);
+    if (title && post_url) {
+        const response = await fetch(`/api/posts`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                post_url
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-    const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            post_url
-        }),
-        headers: {
-            'Content_Type': 'application/json'
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
         }
-    });
-
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
     }
+
 }
 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
